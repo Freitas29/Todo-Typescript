a27d2c0853e86f528bac5b0bcc529d276e22dfd2
Freitas29 <arao.freitas@hotmail.com>
2020-01-22 22:43:38 -0300

    input component

---
+++ src/components/Input.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    v-on="$listeners"
+    :value="value"
+    @input="$emit('update', $event.target.value)"
+  />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    value: String
+  },
+  methods: {
+    handleInput(): void {
+      this.$emit("input", this.value);
+    }
+  },
+  model: {
+    prop: "value",
+    event: "update"
+  }
+});
+</script>
+
+<style scoped>
+.input {
+  width: auto;
+}
+</style>
